17ae09fcded1c4ece8c1884c1a422c5195c638d0
Max DeLiso <maxdeliso@gmail.com>
2011-11-28 20:36:26 -0500

    incremented version number for no good reason

--- inc/globals.h
+++ inc/globals.h
@@ -22,7 +22,7 @@
 #include <QtGlobal>
 #include <QtDebug>
 
-const static int VERSION_ID[3] = {0,0,4};
+const static int VERSION_ID[3] = {0,0,5};
 const static int FLAGS_VALID    = 1;
 const static int FLAGS_VERBOSE  = 2;
 const static int FLAGS_HEADLESS = 4;
